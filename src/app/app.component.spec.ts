65789a386b71f0f02f49a567dae1aa90f7db8cb4
Diarmaid McGowan <diarmaidm@users.noreply.github.com>
2018-01-04 20:21:36 +0000

    fix failing tests - add component import to spec file

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,13 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { StockComponent } from './stock/stock.component';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        StockComponent
       ],
     }).compileComponents();
   }));
